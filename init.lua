9895df0cc87b7a686912c98ae15a5f2fd7d886ae
ma-cel <marcel@xupx.de>
2025-01-10 18:31:54 +0100

    change theme

--- init.lua
+++ init.lua
@@ -618,6 +618,12 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        intelephense = {
+          files = {
+            maxSize = 5000000,
+          },
+          capabilities = require('cmp_nvim_lsp').default_capabilities(),
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -840,7 +846,8 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rose-pine/nvim',
+    name = 'rose-pine',
     opts = {
       transparent = true,
       styles = {
@@ -852,7 +859,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      -- vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'rose-pine'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
